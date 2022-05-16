e5efaeac91d575ca6f1bdbda01befc96036dcb62
MIshmail <michaely.ishmail@gmail.com>
2022-05-15 20:03:34 -0400

    added dates

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -9,12 +9,27 @@ button.onclick = function(){
 
   $.ajax({
   type:"GET",
-  url:"https://api.openweathermap.org/data/2.5/weather?q=" + JSON.parse(city) + "&appid=f258fa1fe74a2f63495e8301ade24835",
+  url:"https://api.openweathermap.org/data/2.5/weather?q=" + JSON.parse(city) + "&units=imperial&appid=f258fa1fe74a2f63495e8301ade24835",
   async:true,
   dataType: "json",
-  success: function(json) {
-              console.log(json)
+  success: function(data) {
+              console.log(data)
+
+              var today = dayjs().format('MM/DD/YYYY')
+              var tomorrow = dayjs().add(1, 'day').format('MM/DD/YYYY')
+              var tomorrow2 = dayjs().add(2, 'day').format('MM/DD/YYYY')
+              var tomorrow3 = dayjs().add(3, 'day').format('MM/DD/YYYY')
+              var tomorrow4 = dayjs().add(4, 'day').format('MM/DD/YYYY')
+              var tomorrow5 = dayjs().add(5, 'day').format('MM/DD/YYYY')
               
+              $('.day-0-date').empty().append(today)
+              $('.day-1-date').empty().append(tomorrow)
+              $('.day-2-date').empty().append(tomorrow2)
+              $('.day-3-date').empty().append(tomorrow3)
+              $('.day-4-date').empty().append(tomorrow4)
+              $('.day-5-date').empty().append(tomorrow5)
+              
+
               // Parse the response.
               // Do other things.
            },
